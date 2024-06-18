18157e7774df0e2f9a74758db52a082a518abc40
shneidmiller <v.shneidmiller@gmail.com>
2024-06-18 13:01:51 +0100

    hello

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# pypi
+# pypi-slava
+A test tool. Don't install it
+
+## Installation
+
+```bash
+pip install pypi-slava
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pypi-slava",
+    version="0.0.1",
+    description="test tool",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Viacheslav Shneidmillier",
+    author_email="v.shneidmiller@gmail.com",
+    url="https://github.com/vshneidmiller/pypi",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "PyYAML",
+        "robotframework"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'pypi-slava=main',
+        ],
+    },
+    python_requires='>=3.6',
+)
