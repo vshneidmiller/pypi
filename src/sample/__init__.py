9d6f41bd037a2e5f09207d060fc72a70ebd7eb9a
shneidmiller <v.shneidmiller@gmail.com>
2024-06-18 13:28:41 +0100

    commented

--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -1,6 +1,6 @@
-from main import app
+#from main import app
 
 def main():
     """Entry point for the application script"""
     print("Call your main application code here")
-    app()
+    #app()
