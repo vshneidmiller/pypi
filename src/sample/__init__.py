664e5e76844b2a9b243d3a29cc810f604091ea56
shneidmiller <v.shneidmiller@gmail.com>
2024-06-18 13:43:17 +0100

    7

--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -1,6 +1,5 @@
-#from main import app
-
 def main():
     """Entry point for the application script"""
     print("Call your main application code here")
-    #app()
+    from lalala import app
+    app()
