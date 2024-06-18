7f2da674250399a9b21e2380b96d44706091ef2a
shneidmiller <v.shneidmiller@gmail.com>
2024-06-18 13:53:36 +0100

    8

--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -1,5 +1,5 @@
 def main():
     """Entry point for the application script"""
-    print("Call your main application code here")
-    from lalala import app
-    app()
+    print("0.0.8")
+    from sample.lalala import app
+    lalala.app()
