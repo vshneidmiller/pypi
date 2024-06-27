194341be1d6f68b764ed827cf8daa949e7ba7141
vshneidmiller <v.shneidmiller@gmail.com>
2024-06-27 16:13:59 +0100

    Refactor __init__.py to remove unused code

--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -1,6 +1,3 @@
-from sample.functions import *
-from sample.main import main
-
-def main():
-    """Entry point for the application script"""
-    main()
+#def main():
+#    """Entry point for the application script"""
+#    main()
