a26feed39b113abc9e33f049ec66de62a28d1356
vshneidmiller <v.shneidmiller@gmail.com>
2024-06-27 16:07:11 +0100

    chore: Update main.py to fix import issue

--- src/sample/main.py
+++ src/sample/main.py
@@ -1,4 +1,11 @@
-from functions import say_my_name
+if __name__ == "__main__" and __package__ is None:
+    import sys
+    from os import path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+    __package__ = "sample"
+
+
+from sample.functions import say_my_name
 
 def main():
     print("main function is executed")
