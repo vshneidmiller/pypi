2fe240041697cb50d98d445de0622fd78dba23cf
vshneidmiller <v.shneidmiller@gmail.com>
2024-06-27 14:40:05 +0100

    anything

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pypi-slava",
-    version="0.0.11",
+    version="0.0.12",
     description="test tool",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -38,7 +38,7 @@ setup(
     # executes the function `main` from this package when invoked:
     entry_points={  # Optional
         "console_scripts": [
-            "pypi-slava=sample:main",
+            "anything=sample:main",
         ],
     },
     python_requires='>=3.6',
--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -1,3 +1,5 @@
+from functions import *
+
 def main():
     """Entry point for the application script"""
     from sample.main import main
